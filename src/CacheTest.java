125136097545eef2ec38ec79e34e0bd21d004e0b
Ted Clifford <edwardfclifford@gmail.com>
2019-11-01 20:39:05 -0400

    Added a test DataProvider

--- src/CacheTest.java
+++ src/CacheTest.java
@@ -6,6 +6,40 @@ import org.junit.Test;
  * Code to test an <tt>LRUCache</tt> implementation.
  */
 public class CacheTest {
+    /**
+     * Implements a DataProvider that can be asked for keys of value (0..4).
+     */
+    public static class TestDataProvider implements DataProvider<Integer, String> {
+
+        TestDataProvider () {}
+
+        public String get (Integer key) {
+            switch (key) {
+                case 0:
+                    return "Value for key 0.";
+                case 1:
+                    return "Value for key 1.";
+                case 2:
+                    return "Value for key 2.";
+                case 3:
+                    return "Value for key 3.";
+                case 4:
+                    return "Value for key 4.";
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Confirms that the TestDataProvider class returns what is expected.
+     */
+    @Test
+    public void testTestDataProviderWorks() {
+        DataProvider<Integer, String> provider = new TestDataProvider();
+        String testValue = provider.get(0);
+        assertTrue(testValue == "Value for key 0.");
+    }
+
 	@Test
 	public void leastRecentlyUsedIsCorrect () {
 		DataProvider<Integer,String> provider = null; // Need to instantiate an actual DataProvider
